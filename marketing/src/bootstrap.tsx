80eea8b268c1c037b5395e69ffce0b61741b9c9a
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-06 14:03:21 -0400

    set up init marketing structure and components

--- marketing/src/bootstrap.tsx
+++ marketing/src/bootstrap.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./App";
 
 const mount = (el: Element) => {
-  ReactDOM.render(
-    <div>
-      <h1>Marketing App</h1>
-    </div>,
-    el
-  );
+  ReactDOM.render(<App />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
