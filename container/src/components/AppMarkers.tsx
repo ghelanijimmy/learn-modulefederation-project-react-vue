b9d4540f4c3115e875bd3db2b923c672beda67d2
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-06 16:06:17 -0400

    fix app wrapper and marker styling

--- container/src/components/AppMarkers.tsx
+++ container/src/components/AppMarkers.tsx
@@ -6,20 +6,18 @@ export const AppMarkers: FC<{ appTitle: string }> = ({
 }) => {
   return (
     <div style={{ border: "1px solid black", position: "relative" }}>
-      <div>
-        <div
-          style={{
-            position: "sticky",
-            top: "0",
-            right: "1em",
-            background: "#333",
-            color: "white",
-            padding: "1em",
-            textAlign: "center",
-          }}
-        >
-          {appTitle}
-        </div>
+      <div
+        style={{
+          position: "sticky",
+          top: "0",
+          right: "1em",
+          background: "#333",
+          color: "white",
+          padding: "1em",
+          textAlign: "center",
+        }}
+      >
+        {appTitle}
       </div>
       {children}
     </div>
