30b71da482e14632ed3a86f34f9a35228eaf3125
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-08 10:18:20 -0400

    Feature/GitHub actions (#20)
    * test cache
    
    * test action run
    
    * git hub action cache config update branch name
    
    * test action run
    
    * github actions change
    
    * test setup node caching
    
    * test setup node caching
    
    * test setup node caching
    
    * test setup node caching
    
    * test setup node caching
    
    * test setup node caching
    
    * update cache action version
    
    * test git hub actions update config for packagejson
    
    * fix git actions
    
    * fix git actions
    
    * fix git actions
    
    * rename webpack files to js instead of ts
    
    * rename webpack files to js instead of ts
    
    * rename webpack files to js instead of ts
    
    * fix marketing action
    
    * fix marketing action
    
    * fix marketing action
    
    * fix marketing action
    
    * fix marketing action
    
    * fix loading of container app

--- container/src/bootstrap.tsx
+++ container/src/bootstrap.tsx
@@ -2,16 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-const mount = (el: Element) => {
-  ReactDOM.render(<App />, el);
-};
-
-if (process.env.NODE_ENV === "development") {
-  const el = document.querySelector("#_container-dev-root");
-
-  if (el) {
-    mount(el);
-  }
-}
-
-export { mount };
+ReactDOM.render(<App />, document.querySelector("#root"));
