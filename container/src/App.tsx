b225159d09a6ddb91dc0e6f9a5176a9ee6cccef0
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-08 15:44:43 -0400

    Feature/css in microfrontends (#21)
    * init add header test
    
    * change actions to also run on pr to main
    
    * fix css collision
    
    * fix css collision
    
    * history fallback fix

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { MarketingApp } from "./components/MarketingApp";
 import { AppMarkers } from "./components/AppMarkers";
+import { Header } from "./components/Header";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export const App = () => {
   return (
-    <div>
-      <AppMarkers appTitle="MarketingApp">
-        <MarketingApp />
-      </AppMarkers>
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <Header signedIn={false} onSignOut={() => null} />
+        <AppMarkers appTitle="MarketingApp">
+          <MarketingApp />
+        </AppMarkers>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
