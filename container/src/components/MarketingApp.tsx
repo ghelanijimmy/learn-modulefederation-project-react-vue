b43f400c4596bf26370c85932327290a8015ff3b
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-09 12:01:49 -0400

    pass routing change up to container

--- container/src/components/MarketingApp.tsx
+++ container/src/components/MarketingApp.tsx
@@ -1,11 +1,18 @@
 import React, { useRef, useEffect } from "react";
 import { mount as MarketingMount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    MarketingMount(ref.current);
+    MarketingMount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) history.push(nextPathname);
+      },
+    });
   }, []);
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.tsx
+++ marketing/src/bootstrap.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, LocationListener } from "history";
 import { App } from "./App";
 
-const mount = (el: Element) => {
-  ReactDOM.render(<App />, el);
+export type MarketingMountFunction = (
+  el: Element,
+  options?: { onNavigate: LocationListener }
+) => void;
+
+const mount = (el: Element, options?: { onNavigate: LocationListener }) => {
+  const history = createMemoryHistory();
+
+  if (options?.onNavigate) {
+    history.listen(options.onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
