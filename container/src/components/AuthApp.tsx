097598bb52b60a4f2ffe5ad7e7c126e80ac659ab
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-12 16:50:59 -0400

    testing type include from workspace

--- container/src/components/AuthApp.tsx
+++ container/src/components/AuthApp.tsx
@@ -1,20 +1,24 @@
 import React, { useRef, useEffect } from 'react';
 import { mount as AuthMount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
+import { AuthMountFunction } from '@mf/auth/src/bootstrap';
 
 export const AuthApp = (props: { onSignIn: () => void }) => {
 	const ref = useRef(null);
 	const history = useHistory();
 
 	useEffect(() => {
-		const { onParentNavigate } = AuthMount(ref.current, {
-			onNavigate: ({ pathname: nextPathname }) => {
-				const { pathname } = history.location;
-				if (pathname !== nextPathname) history.push(nextPathname);
+		const { onParentNavigate } = (AuthMount as AuthMountFunction)(
+			ref.current,
+			{
+				onNavigate: ({ pathname: nextPathname }) => {
+					const { pathname } = history.location;
+					if (pathname !== nextPathname) history.push(nextPathname);
+				},
+				initialPath: history.location.pathname,
+				onSignIn: props.onSignIn,
 			},
-			initialPath: history.location.pathname,
-			onSignIn: props.onSignIn,
-		});
+		);
 		history.listen(onParentNavigate);
 	}, []);
 
