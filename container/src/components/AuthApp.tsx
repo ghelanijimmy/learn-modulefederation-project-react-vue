554c24b63461e8f2ec5a80d837104e7635ddd5d1
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-04-12 16:39:16 -0400

    fix type issues

--- container/src/components/AuthApp.tsx
+++ container/src/components/AuthApp.tsx
@@ -1,24 +1,20 @@
 import React, { useRef, useEffect } from 'react';
 import { mount as AuthMount } from 'auth/AuthApp';
 import { useHistory } from 'react-router-dom';
-import { AuthMountFunction } from '@mf/auth/dist/src/bootstrap';
 
 export const AuthApp = (props: { onSignIn: () => void }) => {
 	const ref = useRef(null);
 	const history = useHistory();
 
 	useEffect(() => {
-		const { onParentNavigate } = (AuthMount as AuthMountFunction)(
-			ref.current,
-			{
-				onNavigate: ({ pathname: nextPathname }) => {
-					const { pathname } = history.location;
-					if (pathname !== nextPathname) history.push(nextPathname);
-				},
-				initialPath: history.location.pathname,
-				onSignIn: props.onSignIn,
+		const { onParentNavigate } = AuthMount(ref.current, {
+			onNavigate: ({ pathname: nextPathname }) => {
+				const { pathname } = history.location;
+				if (pathname !== nextPathname) history.push(nextPathname);
 			},
-		);
+			initialPath: history.location.pathname,
+			onSignIn: props.onSignIn,
+		});
 		history.listen(onParentNavigate);
 	}, []);
 
